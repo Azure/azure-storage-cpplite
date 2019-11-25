a61fff3ec9dc4829c54e3dcb8f771f1e42ae7ccc
Jinming Hu <jinmhu@microsoft.com>
2019-11-25 17:51:01 +0800

    Add switch for dllimport/dllexport

--- include/storage_EXPORTS.h
+++ include/storage_EXPORTS.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #ifdef _WIN32
+#ifdef azure_storage_lite_EXPORTS
 #define AZURE_STORAGE_API __declspec(dllexport)
+#else
+#define AZURE_STORAGE_API __declspec(dllimport)
+#endif
 #else /* ifdef _WIN32 */
 #define AZURE_STORAGE_API
 #endif
