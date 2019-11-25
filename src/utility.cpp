4bdcfe1c1d5b2b2b3eb179148e2eccc8de8f3aef
Jinming Hu <jinmhu@microsoft.com>
2019-11-25 17:40:55 +0800

    Move URL-encode code to utility so that ADLS Gen2 can leverage it

--- src/utility.cpp
+++ src/utility.cpp
@@ -14,6 +14,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #endif
+#include <vector>
 
 namespace azure {  namespace storage_lite {
 
@@ -151,6 +152,84 @@ namespace azure {  namespace storage_lite {
             return false;
         }
         return true;
+    }
+
+    static const char* unreserved = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~";
+    static const char* subdelimiters = "!$&'()*+,;=";
+    static const char* encoded_chars[] = {
+        "%00", "%01", "%02", "%03", "%04", "%05", "%06", "%07", "%08", "%09", "%0A", "%0B", "%0C", "%0D", "%0E", "%0F",
+        "%10", "%11", "%12", "%13", "%14", "%15", "%16", "%17", "%18", "%19", "%1A", "%1B", "%1C", "%1D", "%1E", "%1F",
+        "%20", "%21", "%22", "%23", "%24", "%25", "%26", "%27", "%28", "%29", "%2A", "%2B", "%2C", "%2D", "%2E", "%2F",
+        "%30", "%31", "%32", "%33", "%34", "%35", "%36", "%37", "%38", "%39", "%3A", "%3B", "%3C", "%3D", "%3E", "%3F",
+        "%40", "%41", "%42", "%43", "%44", "%45", "%46", "%47", "%48", "%49", "%4A", "%4B", "%4C", "%4D", "%4E", "%4F",
+        "%50", "%51", "%52", "%53", "%54", "%55", "%56", "%57", "%58", "%59", "%5A", "%5B", "%5C", "%5D", "%5E", "%5F",
+        "%60", "%61", "%62", "%63", "%64", "%65", "%66", "%67", "%68", "%69", "%6A", "%6B", "%6C", "%6D", "%6E", "%6F",
+        "%70", "%71", "%72", "%73", "%74", "%75", "%76", "%77", "%78", "%79", "%7A", "%7B", "%7C", "%7D", "%7E", "%7F",
+        "%80", "%81", "%82", "%83", "%84", "%85", "%86", "%87", "%88", "%89", "%8A", "%8B", "%8C", "%8D", "%8E", "%8F",
+        "%90", "%91", "%92", "%93", "%94", "%95", "%96", "%97", "%98", "%99", "%9A", "%9B", "%9C", "%9D", "%9E", "%9F",
+        "%A0", "%A1", "%A2", "%A3", "%A4", "%A5", "%A6", "%A7", "%A8", "%A9", "%AA", "%AB", "%AC", "%AD", "%AE", "%AF",
+        "%B0", "%B1", "%B2", "%B3", "%B4", "%B5", "%B6", "%B7", "%B8", "%B9", "%BA", "%BB", "%BC", "%BD", "%BE", "%BF",
+        "%C0", "%C1", "%C2", "%C3", "%C4", "%C5", "%C6", "%C7", "%C8", "%C9", "%CA", "%CB", "%CC", "%CD", "%CE", "%CF",
+        "%D0", "%D1", "%D2", "%D3", "%D4", "%D5", "%D6", "%D7", "%D8", "%D9", "%DA", "%DB", "%DC", "%DD", "%DE", "%DF",
+        "%E0", "%E1", "%E2", "%E3", "%E4", "%E5", "%E6", "%E7", "%E8", "%E9", "%EA", "%EB", "%EC", "%ED", "%EE", "%EF",
+        "%F0", "%F1", "%F2", "%F3", "%F4", "%F5", "%F6", "%F7", "%F8", "%F9", "%FA", "%FB", "%FC", "%FD", "%FE", "%FF"
+    };
+    std::string encode_url_path(const std::string& path)
+    {
+        static const std::vector<uint8_t> is_path_char = []()
+        {
+            std::vector<uint8_t> ret(256, 0);
+            for (char c : std::string(unreserved) + std::string(subdelimiters) + "%!@")
+            {
+                ret[c] = 1;
+            }
+            // Parameter path is already joint with '/'.
+            ret['/'] = 1;
+            return ret;
+        }();
+
+        std::string result;
+        for (char c : path)
+        {
+            if (is_path_char[c])
+            {
+                result += c;
+            }
+            else
+            {
+                result += encoded_chars[static_cast<unsigned char>(c)];
+            }
+        }
+
+        return result;
+    }
+
+    std::string encode_url_query(const std::string& query)
+    {
+        static const std::vector<uint8_t> is_query_char = []()
+        {
+            std::vector<uint8_t> ret(256, 0);
+            for (char c : std::string(unreserved) + std::string(subdelimiters) + "%!@/?")
+            {
+                ret[c] = 1;
+            }
+            return ret;
+        }();
+
+        std::string result;
+        for (char c : query)
+        {
+            if (is_query_char[c])
+            {
+                result += c;
+            }
+            else
+            {
+                result += encoded_chars[static_cast<unsigned char>(c)];
+            }
+        }
+
+        return result;
     }
 
 }}   // azure::storage_lite
