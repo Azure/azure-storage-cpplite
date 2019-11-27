811704d7296ba074c6215b277735edc0e94fd4f2
Jinming Hu <jinmhu@microsoft.com>
2019-11-27 16:57:31 +0800

    ADLS Gen2 implementation

--- adls/include/adls_client.h
+++ adls/include/adls_client.h
@@ -1,73 +1,102 @@
 #pragma once
 
+#ifdef _WIN32
+#ifdef azure_storage_adls_EXPORTS
+#define AZURE_STORAGE_ADLS_API __declspec(dllexport)
+#else
+#define AZURE_STORAGE_ADLS_API __declspec(dllimport)
+#endif
+#else /* ifdef _WIN32 */
+#define AZURE_STORAGE_ADLS_API
+#endif
+
+#include <stdexcept>
+
 #include "blob/blob_client.h"
+#include "set_access_control_request.h"
+#include "list_paths_request.h"
 
 namespace azure { namespace storage_adls {
     using storage_account = azure::storage_lite::storage_account;
     using executor_context = azure::storage_lite::executor_context;
 
-    struct list_filesystem_item
+    struct list_filesystems_item
     {
         std::string name;
-        // Other properties
     };
 
-    struct list_filesystem_result
+    struct list_filesystems_result
     {
-        std::vector<list_filesystem_item> filesystems;
+        std::vector<list_filesystems_item> filesystems;
         std::string continuation_token;
     };
 
-    struct list_path_item
+    struct storage_exception : public std::exception
     {
-        std::string name;
-        // Other properties
-    };
+        storage_exception(int code, std::string code_name, std::string message) : code(code), code_name(std::move(code_name)), message(std::move(message)) {}
 
-    struct list_path_result
-    {
-        std::vector<list_path_item> paths;
-        std::string continuation_token;
+        int code;
+        std::string code_name;
+        std::string message;
     };
 
     class adls_client final
     {
     public:
-        adls_client(std::shared_ptr<storage_account> account, int max_concurrency) : m_account(account) {}
+        AZURE_STORAGE_ADLS_API adls_client(std::shared_ptr<storage_account> account, int max_concurrency, bool exception_enabled = true);
 
-        void create_filesystem(const std::string& filesystem);
-        void delete_filesystem(const std::string& filesystem);
-        bool filesystem_exists(const std::string& filesystem);
-        void set_filesystem_properties(const std::string& filesystem, const std::vector<std::pair<std::string, std::string>>& properties);
-        std::vector<std::pair<std::string, std::string>> get_filesystem_properties(const std::string& filesystem);
-        list_filesystem_result list_filesystems_segmented(const std::string& prefix, const std::string& continuation_token = std::string(), const int max_results = 5);
+        AZURE_STORAGE_ADLS_API void create_filesystem(const std::string& filesystem);
+        AZURE_STORAGE_ADLS_API void delete_filesystem(const std::string& filesystem);
+        AZURE_STORAGE_ADLS_API bool filesystem_exists(const std::string& filesystem);
+        AZURE_STORAGE_ADLS_API void set_filesystem_properties(const std::string& filesystem, const std::vector<std::pair<std::string, std::string>>& properties);
+        AZURE_STORAGE_ADLS_API std::vector<std::pair<std::string, std::string>> get_filesystem_properties(const std::string& filesystem);
+        AZURE_STORAGE_ADLS_API list_filesystems_result list_filesystems_segmented(const std::string& prefix, const std::string& continuation_token = std::string(), const int max_results = 0);
 
-        void create_directory(const std::string& filesystem, const std::string& directory);
-        void delete_directory(const std::string& filesystem, const std::string& directory);
-        bool directory_exists(const std::string& filesystem, const std::string& directory);
-        void set_directory_properties(const std::string& filesystem, const std::string& directory, const std::vector<std::pair<std::string, std::string>>& properties);
-        std::vector<std::pair<std::string, std::string>> get_directory_properties(const std::string& filesystem, const std::string& directory);
-        void set_directory_access_control(const std::string& filesystem, const std::string& directory, const std::string& acl);
-        std::string get_directory_access_control(const std::string& filesystem, const std::string& directory);
-        list_path_result list_paths_segmented(const std::string& filesystem, const std::string& directory, bool recursive = false, const std::string& continuation_token = std::string(), const int max_results = 5);
+        AZURE_STORAGE_ADLS_API void create_directory(const std::string& filesystem, const std::string& directory);
+        AZURE_STORAGE_ADLS_API void delete_directory(const std::string& filesystem, const std::string& directory);
+        AZURE_STORAGE_ADLS_API bool directory_exists(const std::string& filesystem, const std::string& directory);
+        AZURE_STORAGE_ADLS_API void move_directory(const std::string& filesystem, const std::string& source_path, const std::string& destination_path);
+        AZURE_STORAGE_ADLS_API void move_directory(const std::string& source_filesystem, const std::string& source_path, const std::string& destination_filesystem, const std::string& destination_path);
+        AZURE_STORAGE_ADLS_API void set_directory_properties(const std::string& filesystem, const std::string& directory, const std::vector<std::pair<std::string, std::string>>& properties);
+        AZURE_STORAGE_ADLS_API std::vector<std::pair<std::string, std::string>> get_directory_properties(const std::string& filesystem, const std::string& directory);
+        AZURE_STORAGE_ADLS_API void set_directory_access_control(const std::string& filesystem, const std::string& directory, const access_control& acl);
+        AZURE_STORAGE_ADLS_API access_control get_directory_access_control(const std::string& filesystem, const std::string& directory);
+        AZURE_STORAGE_ADLS_API list_paths_result list_paths_segmented(const std::string& filesystem, const std::string& directory, bool recursive = false, const std::string& continuation_token = std::string(), const int max_results = 0);
 
-        void append_data_from_stream(const std::string& filesystem, const std::string& file, size_t offset, std::istream& istream);
-        void flush_data(const std::string& filesystem, const std::string& file, size_t offset);
-        void upload_file_from_stream(const std::string& filesystem, const std::string& file, std::istream& in_stream, const std::vector<std::pair<std::string, std::string>>& properties = std::vector<std::pair<std::string, std::string>>());
-        void download_file_to_stream(const std::string& filesystem, const std::string& file, std::ostream& out_stream);
-        void download_file_to_stream(const std::string& filesystem, const std::string& file, size_t offset, size_t size, std::ostream& out_stream);
-        void delete_file(const std::string& filesystem, const std::string& file);
-        bool file_exists(const std::string& filesystem, const std::string& file);
-        void set_file_properties(const std::string& filesystem, const std::string& file, const std::vector<std::pair<std::string, std::string>>& properties);
-        std::vector<std::pair<std::string, std::string>> get_file_properties(const std::string& filesystem, const std::string& file);
-        void set_file_access_control(const std::string& filesystem, const std::string& file, const std::string& acl);
-        std::string get_file_access_control(const std::string& filesystem, const std::string& file);
+        AZURE_STORAGE_ADLS_API void create_file(const std::string& filename, const std::string& file);
+        AZURE_STORAGE_ADLS_API void append_data_from_stream(const std::string& filesystem, const std::string& file, uint64_t offset, std::istream& in_stream, uint64_t stream_len = 0);
+        AZURE_STORAGE_ADLS_API void flush_data(const std::string& filesystem, const std::string& file, uint64_t offset);
+        AZURE_STORAGE_ADLS_API void upload_file_from_stream(const std::string& filesystem, const std::string& file, std::istream& in_stream, const std::vector<std::pair<std::string, std::string>>& properties = std::vector<std::pair<std::string, std::string>>());
+        AZURE_STORAGE_ADLS_API void download_file_to_stream(const std::string& filesystem, const std::string& file, std::ostream& out_stream);
+        AZURE_STORAGE_ADLS_API void download_file_to_stream(const std::string& filesystem, const std::string& file, uint64_t offset, uint64_t size, std::ostream& out_stream);
+        AZURE_STORAGE_ADLS_API void delete_file(const std::string& filesystem, const std::string& file);
+        AZURE_STORAGE_ADLS_API bool file_exists(const std::string& filesystem, const std::string& file);
+        AZURE_STORAGE_ADLS_API void move_file(const std::string& filesystem, const std::string& source_path, const std::string& destination_path);
+        AZURE_STORAGE_ADLS_API void move_file(const std::string& source_filesystem, const std::string& source_path, const std::string& destination_filesystem, const std::string& destination_path);
+        AZURE_STORAGE_ADLS_API void set_file_properties(const std::string& filesystem, const std::string& file, const std::vector<std::pair<std::string, std::string>>& properties);
+        AZURE_STORAGE_ADLS_API std::vector<std::pair<std::string, std::string>> get_file_properties(const std::string& filesystem, const std::string& file);
+        AZURE_STORAGE_ADLS_API void set_file_access_control(const std::string& filesystem, const std::string& file, const access_control& acl);
+        AZURE_STORAGE_ADLS_API access_control get_file_access_control(const std::string& filesystem, const std::string& file);
+
+        bool exception_enabled() const
+        {
+            return m_exception_enabled;
+        }
+    private:
+        template<class RET, class FUNC>
+        RET blob_client_adaptor(FUNC func);
+
+        bool success() const
+        {
+            return !(!m_exception_enabled && errno != 0);
+        }
 
     private:
-        std::shared_ptr<azure::storage_lite::blob_client_wrapper> m_blob_client;
+        std::shared_ptr<azure::storage_lite::blob_client> m_blob_client;
         std::shared_ptr<storage_account> m_account;
         std::shared_ptr<executor_context> m_context;
-    };
 
+        const bool m_exception_enabled;
+    };
 
 }}  // azure::storage_adls
