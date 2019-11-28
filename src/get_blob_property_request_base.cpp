91d9eed8f4f6c646775b64f876242454b736c3db
Jinming Hu <jinmhu@microsoft.com>
2019-11-28 18:20:13 +0800

    Fix typo in comments

--- src/get_blob_property_request_base.cpp
+++ src/get_blob_property_request_base.cpp
@@ -23,7 +23,7 @@ namespace azure {  namespace storage_lite {
         storage_headers headers;
 
         add_ms_header(h, headers, constants::header_ms_client_request_id, r.ms_client_request_id(), true);
-        // les is not supported.
+        // lease is not supported.
         // add_ms_header(h, headers, constants::header_ms_lease_id, r.ms_lease_id(), true);
 
         h.add_header(constants::header_user_agent, constants::header_value_user_agent);
