9310837d0bcbac5fe00b4e8a69ac63c9ee67f0a1
Jinming Hu <jinmhu@microsoft.com>
2019-12-30 12:10:27 +0800

    Use different API versions for different services

--- src/list_blobs_request_base.cpp
+++ src/list_blobs_request_base.cpp
@@ -48,7 +48,7 @@ void list_blobs_request_base::build_request(const storage_account &a, http_base 
 
     h.add_header(constants::header_user_agent, constants::header_value_user_agent);
     add_ms_header(h, headers, constants::header_ms_date, get_ms_date(date_format::rfc_1123));
-    add_ms_header(h, headers, constants::header_ms_version, constants::header_value_storage_version);
+    add_ms_header(h, headers, constants::header_ms_version, constants::header_value_storage_blob_version);
 
     a.credential()->sign_request(r, h, url, headers);
 }
@@ -108,7 +108,7 @@ void list_blobs_segmented_request_base::build_request(const storage_account &a, 
 
     h.add_header(constants::header_user_agent, constants::header_value_user_agent);
     add_ms_header(h, headers, constants::header_ms_date, get_ms_date(date_format::rfc_1123));
-    add_ms_header(h, headers, constants::header_ms_version, constants::header_value_storage_version);
+    add_ms_header(h, headers, constants::header_ms_version, constants::header_value_storage_blob_version);
 
     a.credential()->sign_request(r, h, url, headers);
 }
